6b9b767e446cffbb8a41c2b3a6c6b27478f64b08
Enrique <carlosenrique753p@gmail.com>
2023-06-03 17:21:05 -0400

    primerComit

---
+++ AbraaoMarques/NewsletterModal/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    type: \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    componentName: 'AbraaoMarques_NewsletterModal',
+    path: __DIR__
+);
---
+++ Codetheatres/CustomMenu/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Codetheatres_CustomMenu',
+    __DIR__
+);
